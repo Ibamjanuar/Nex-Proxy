0ac538ae6f725df1626ef4c2a2c08f63d9f03bb4
Nex <95665833+Ibamjanuar@users.noreply.github.com>
2024-02-16 13:52:25 +0700

    Update catupdate.sh

--- catupdate.sh
+++ catupdate.sh
@@ -5,9 +5,9 @@ ENDCOLOR="\e[0m"
 clear
 echo -e "${GREEN}Updating Proxy...${ENDCOLOR}"
 sleep 1
-if [ -f "arunika" ]; then
+if [ -f "nex" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
-    rm arunika
+    rm nex
     sleep 1
     echo -e "${GREEN}Updating proxy...${ENDCOLOR}"
 fi
@@ -19,8 +19,8 @@ pkg install curl -y
 pkg install libenet -y
 pkg install wget -y
 clear
-wget -q https://github.com/AmertaByte/Arunika-Proxy/raw/main/arunika
+wget -q https://github.com/Ibamjanuar/Nex-Proxy/raw/main/nex
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
-echo -e "${GREEN}Execute proxy with this command: ./arunika${ENDCOLOR}"
-chmod +x arunika
+echo -e "${GREEN}Execute proxy with this command: ./nex${ENDCOLOR}"
+chmod +x nex
